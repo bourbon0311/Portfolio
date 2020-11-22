2d849db7fdeae4237fd1ca34ccc64fc532c7e5cd
vagrant <vagrant@localhost.localdomain>
2020-11-22 06:37:37 +0000

    db reset

--- db/migrate/20201118105950_add_name_to_tags.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToTags < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tags, :name, :string
-    add_column :tags, :food_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_105950) do
+ActiveRecord::Schema.define(version: 2020_11_18_105456) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -39,11 +39,6 @@ ActiveRecord::Schema.define(version: 2020_11_18_105950) do
     t.string "food_image_id"
   end
 
-  create_table "ranks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -55,10 +50,10 @@ ActiveRecord::Schema.define(version: 2020_11_18_105950) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.integer "food_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
